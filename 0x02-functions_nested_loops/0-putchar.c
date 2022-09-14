2f682041debbff054d1dc273e23905e67c751083
Owad24 <ayushau24@outlook.com>
2022-09-13 17:03:28 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <main.h>
+
+/** main - prints "_putchar", followed by a new line
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n);
+
+	return (0);
+}	
